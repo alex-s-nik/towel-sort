c63f4aa2116fdaf1e64fa226ea5dba7ba601fba7
alex-s-nik <to_ctrl@mail.ru>
2021-03-08 21:16:42 +0300

    fix: second fix

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ module.exports = function towelSort (matrix) {
   let arr = [];
 
   for (let i=0; i<matrix.length; i++){
-    for (let j=0; j<matrix[i].length; j++){
-      arr.push(matrix[i][j]);
+    if (i%2) {
+      for (let j=0; j<matrix[i].length; j++){
+        arr.push(matrix[i][j]);
+      }
     }
+    else{
+      for (let j=matrix[i].length-1; j>=0; j--){
+        arr.push(matrix[i][j]);
+      }
+    }
+    
   }
-  return arr.sort().reverse();
+  return arr;
 }
